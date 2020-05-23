2e49b9d932b96c3a67ce01edc5783e8a3c1d4d4d
amyth74 <acahryaamit74@gmail.com>
2020-05-23 21:35:57 +0545

    relation for profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -14,4 +14,8 @@ class Profile extends Model
         'user_id',
         'citizenship_number',
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->hasMany(Booking::class);
     }
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
 }
